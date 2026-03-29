4764cb1a6d8ace6a89eee431a92054d92603ef48
mc704ed <mykyta.churylov@student.tuke.sk>
2026-03-29 12:00:57 +0200

    ff

--- src/main/kotlin/mate/academy/ProcessAndFilterNumbers.kt
+++ src/main/kotlin/mate/academy/ProcessAndFilterNumbers.kt
@@ -1,5 +1,10 @@
 package mate.academy
 
-fun processAndFilterNumbers(numbers: List<Int>): List<Int> {
+const val NUMBER = 2
+const val BORDER = 25
 
+fun processAndFilterNumbers(numbers: List<Int>): List<Int> {
+    return numbers
+        .map { number -> if (number  % NUMBER != 0) number * NUMBER else number / NUMBER}
+        .filter { it > BORDER }
 }
